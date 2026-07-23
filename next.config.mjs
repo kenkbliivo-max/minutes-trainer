ba1c901f575e94d0a99e424eebfa30fc8bbc056a
kenkbliivo-max <kenkbliivo@gmail.com>
2026-07-24 02:05:14 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["pg", "pdfjs-dist", "bcryptjs"],
     outputFileTracingIncludes: {
-      "/api/submissions": [
-        "./node_modules/pdfjs-dist/cmaps/**/*",
-        "./node_modules/pdfjs-dist/standard_fonts/**/*",
-      ],
+      "/api/submissions": ["./node_modules/pdfjs-dist/**/*"],
     },
   },
 };
